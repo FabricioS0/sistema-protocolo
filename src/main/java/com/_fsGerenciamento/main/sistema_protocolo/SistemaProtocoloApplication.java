5fbf3aa43ab85965c69a9043f7e3ae211d2915ee
FabricioS0 <fsoliveira899@gmail.com>
2025-06-03 12:20:26 -0300

    first commit

---
+++ src/main/java/com/_fsGerenciamento/main/sistema_protocolo/SistemaProtocoloApplication.java
@@ -0,0 +1,13 @@
+package com._fsGerenciamento.main.sistema_protocolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaProtocoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaProtocoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_fsGerenciamento/main/sistema_protocolo/SistemaProtocoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com._fsGerenciamento.main.sistema_protocolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaProtocoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
